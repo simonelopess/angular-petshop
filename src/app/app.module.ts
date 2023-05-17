0b1d2e6801c520c9bba7557dd4ed1d5db1c33ef0
Simone Lopes <sisilopes20@gmail.com>
2023-05-17 22:37:39 +0100

    create Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignUpPageComponent
   ],
   imports: [
     BrowserModule,
