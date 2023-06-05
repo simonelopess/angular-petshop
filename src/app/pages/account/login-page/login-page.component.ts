8d9e8e1232d37e58eb2907ed8c6d2154b4c43aeb
Simone Lopes <sisilopes20@gmail.com>
2023-06-05 19:48:32 +0100

    adiciona formulário de login

--- src/app/pages/account/login-page/login-page.component.ts
+++ src/app/pages/account/login-page/login-page.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  public form: FormGroup;
 
+  constructor(
+    private service: DataService,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      username: ['', Validators.compose([
+        Validators.minLength(11),
+        Validators.maxLength(60),
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        Validators.required
+      ])],
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
 }
