592cd4ed79f76dbba376abb38cea62edb7e881c2
Simone Lopes <sisilopes20@gmail.com>
2023-06-05 18:55:48 +0100

    ajustando o grid

--- src/app/components/store/product-card/product-card.component.ts
+++ src/app/components/store/product-card/product-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
-  @Input() product: any;
+  @Input()
+  product!: Product;
 }
